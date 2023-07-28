2427fed7944b434b4839e7660d2a985d82016951
peugeot <thomaspeugeot@yahoo.fr>
2023-07-28 09:35:29 +0200

    Add Firltering Fixes #4

--- ng/projects/gongtablespecific/src/lib/gongtablespecific.module.ts
+++ ng/projects/gongtablespecific/src/lib/gongtablespecific.module.ts
@@ -8,6 +8,9 @@ import { MatTableModule } from '@angular/material/table';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,9 @@ import { MatIconModule } from '@angular/material/icon';
     MatCheckboxModule,
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
   ],
   exports: [
     GithubComFullstackLangGongtableGoDataModelSpecificComponent
