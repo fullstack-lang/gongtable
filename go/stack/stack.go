54fa97371f89565c3daa5568ace20a0900273843
peugeot <thomaspeugeot@yahoo.fr>
2023-11-04 11:42:51 +0100

    new gongc

--- go/stack/stack.go
+++ go/stack/stack.go
@@ -120,7 +120,7 @@ func NewStack(
 	}
 
 	if withProbe {
-		probe.NewProbe(r, gongtable_go.GoModelsDir, gongtable_go.GoDiagramsDir,
+		stack.Probe = probe.NewProbe(r, gongtable_go.GoModelsDir, gongtable_go.GoDiagramsDir,
 			embeddedDiagrams, stackPath, stage, backRepo)
 	}
 
