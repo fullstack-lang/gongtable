2c2f98fa55f31a7d1eb1a60c3af03a6cda9b69c2
peugeot <thomaspeugeot@yahoo.fr>
2025-02-04 15:10:03 +0100

    Issue with sorting Fixes #36

--- go/cmd/gongtable/table_stage.go
+++ go/cmd/gongtable/table_stage.go
@@ -10,283 +10,187 @@ import (
 
 // generated in order to avoid error in the package import
 // if there are no elements in the stage to marshall
-var ___dummy__Stage_table_stage models.StageStruct
-var ___dummy__Time_table_stage time.Time
+var _ time.Time
 
-// _ point for meta package dummy declaration{{ImportPackageDummyDeclaration}}
+// Injection point for meta package dummy declaration{{ImportPackageDummyDeclaration}}
 
-// currently, DocLink renaming is not enabled in gopls
-// the following map are devised to overcome this limitation
-// those maps and the processing code will be eleminated when
-// DocLink renaming will be enabled in gopls
-// [Corresponding Issue](https://github.com/golang/go/issues/57559)
-//
 // When parsed, those maps will help with the renaming process
 var _ map[string]any = map[string]any{
 	// injection point for docLink to identifiers{{EntriesDocLinkStringDocLinkIdentifier}}
 }
 
-// init might be handy if one want to have the data embedded in the binary
-// but it has to properly reference the _ gateway in the main package
-// func init() {
-// 	_ = __Dummy_time_variable
-// 	InjectionGateway["table_stage"] = _
-// }
-
-// _ will stage objects of database "table_stage"
+// function will stage objects
 func _(stage *models.StageStruct) {
 
 	// Declaration of instances to stage
 
-	// Declarations of staged instances of Cell
-	__Cell__000000_Row_1_Cell_1 := (&models.Cell{Name: `Row 1 - Cell 1`}).Stage(stage)
-	__Cell__000001_Row_1_Cell_2 := (&models.Cell{Name: `Row 1 - Cell 2`}).Stage(stage)
-	__Cell__000002_Row_1_Cell_3 := (&models.Cell{Name: `Row 1 - Cell 3`}).Stage(stage)
-	__Cell__000003_Row_1_Cell_4 := (&models.Cell{Name: `Row 1 - Cell 4`}).Stage(stage)
-	__Cell__000004_Row_1_Cell_5 := (&models.Cell{Name: `Row 1 - Cell 5`}).Stage(stage)
-	__Cell__000005_Row_2_Cell_1 := (&models.Cell{Name: `Row 2 - Cell 1`}).Stage(stage)
-	__Cell__000006_Row_2_Cell_2 := (&models.Cell{Name: `Row 2 - Cell 2`}).Stage(stage)
-	__Cell__000007_Row_2_Cell_3 := (&models.Cell{Name: `Row 2 - Cell 3`}).Stage(stage)
-	__Cell__000008_Row_2_Cell_4 := (&models.Cell{Name: `Row 2 - Cell 4`}).Stage(stage)
-	__Cell__000009_Row_2_Cell_5 := (&models.Cell{Name: `Row 2 - Cell 5`}).Stage(stage)
-	__Cell__000010_T2_R1_A := (&models.Cell{Name: `T2 - R1 - A`}).Stage(stage)
-	__Cell__000011_T2_R1_B := (&models.Cell{Name: `T2 - R1 - B`}).Stage(stage)
-	__Cell__000012_T2_R2_A := (&models.Cell{Name: `T2 - R2 - A`}).Stage(stage)
-	__Cell__000013_T2_R2_B := (&models.Cell{Name: `T2 - R2 - B`}).Stage(stage)
-	__Cell__000014_T2_R3_A := (&models.Cell{Name: `T2 - R3 - A`}).Stage(stage)
-	__Cell__000015_T2_R3_B := (&models.Cell{Name: `T2 - R3 - B`}).Stage(stage)
+	__Cell__000000_Row_1_Cell_1 := (&models.Cell{}).Stage(stage)
+	__Cell__000001_Row_1_Cell_2 := (&models.Cell{}).Stage(stage)
+	__Cell__000002_Row_1_Cell_3 := (&models.Cell{}).Stage(stage)
+	__Cell__000003_Row_1_Cell_4 := (&models.Cell{}).Stage(stage)
+	__Cell__000004_Row_1_Cell_5 := (&models.Cell{}).Stage(stage)
+	__Cell__000005_Row_2_Cell_1 := (&models.Cell{}).Stage(stage)
+	__Cell__000006_Row_2_Cell_2 := (&models.Cell{}).Stage(stage)
+	__Cell__000007_Row_2_Cell_3 := (&models.Cell{}).Stage(stage)
+	__Cell__000008_Row_2_Cell_4 := (&models.Cell{}).Stage(stage)
+	__Cell__000009_Row_2_Cell_5 := (&models.Cell{}).Stage(stage)
+	__Cell__000010_T2_R1_A := (&models.Cell{}).Stage(stage)
+	__Cell__000011_T2_R1_B := (&models.Cell{}).Stage(stage)
+	__Cell__000012_T2_R2_A := (&models.Cell{}).Stage(stage)
+	__Cell__000013_T2_R2_B := (&models.Cell{}).Stage(stage)
+	__Cell__000014_T2_R3_A := (&models.Cell{}).Stage(stage)
+	__Cell__000015_T2_R3_B := (&models.Cell{}).Stage(stage)
 
-	// Declarations of staged instances of CellBoolean
-	__CellBoolean__000000_Row_1_Cell_1_Cell_False := (&models.CellBoolean{Name: `Row 1 -Cell 1 - Cell False`}).Stage(stage)
-	__CellBoolean__000001_Row_2_Cell_1_Cell_true := (&models.CellBoolean{Name: `Row 2 - Cell 1 - Cell true`}).Stage(stage)
+	__CellBoolean__000000_Row_1_Cell_1_Cell_False := (&models.CellBoolean{}).Stage(stage)
+	__CellBoolean__000001_Row_2_Cell_1_Cell_true := (&models.CellBoolean{}).Stage(stage)
 
-	// Declarations of staged instances of CellFloat64
-	__CellFloat64__000000_Row_1_Cell2_Float := (&models.CellFloat64{Name: `Row 1 - Cell2 - Float`}).Stage(stage)
-	__CellFloat64__000001_Row_2_Cell_2_Float := (&models.CellFloat64{Name: `Row 2 - Cell 2 - Float`}).Stage(stage)
+	__CellFloat64__000000_Row_1_Cell2_Float := (&models.CellFloat64{}).Stage(stage)
+	__CellFloat64__000001_Row_2_Cell_2_Float := (&models.CellFloat64{}).Stage(stage)
 
-	// Declarations of staged instances of CellIcon
-	__CellIcon__000000_Row_1_Cell_3_Delete := (&models.CellIcon{Name: `Row 1 - Cell 3 - Delete`}).Stage(stage)
-	__CellIcon__000001_Row_2_Cell_3_home := (&models.CellIcon{Name: `Row 2 - Cell 3 - home`}).Stage(stage)
+	__CellIcon__000000_Row_1_Cell_3_Delete := (&models.CellIcon{}).Stage(stage)
+	__CellIcon__000001_Row_2_Cell_3_home := (&models.CellIcon{}).Stage(stage)
 
-	// Declarations of staged instances of CellInt
-	__CellInt__000000_Row_1_Cell_4_Int := (&models.CellInt{Name: `Row 1 - Cell 4 - Int`}).Stage(stage)
-	__CellInt__000001_Row_2_Cell_4_Int := (&models.CellInt{Name: `Row 2 - Cell 4 - Int`}).Stage(stage)
+	__CellInt__000000_Row_1_Cell_4_Int := (&models.CellInt{}).Stage(stage)
+	__CellInt__000001_Row_2_Cell_4_Int := (&models.CellInt{}).Stage(stage)
 
-	// Declarations of staged instances of CellString
-	__CellString__000000_Row_1_Cell_5 := (&models.CellString{Name: `Row 1 - Cell 5`}).Stage(stage)
-	__CellString__000001_Row_2_Cell_5 := (&models.CellString{Name: `Row 2 - Cell 5`}).Stage(stage)
-	__CellString__000002_T2_R1_A := (&models.CellString{Name: `T2 - R1 - A`}).Stage(stage)
-	__CellString__000003_T2_R1_B := (&models.CellString{Name: `T2 - R1 - B`}).Stage(stage)
-	__CellString__000004_T2_R2_A := (&models.CellString{Name: `T2 - R2 - A`}).Stage(stage)
-	__CellString__000005_T2_R2_B := (&models.CellString{Name: `T2 - R2 - B`}).Stage(stage)
-	__CellString__000006_T2_R3_A := (&models.CellString{Name: `T2 - R3 - A`}).Stage(stage)
-	__CellString__000007_T2_R3_B := (&models.CellString{Name: `T2 - R3 - B`}).Stage(stage)
+	__CellString__000000_Row_1_Cell_5 := (&models.CellString{}).Stage(stage)
+	__CellString__000001_Row_2_Cell_5 := (&models.CellString{}).Stage(stage)
+	__CellString__000002_T2_R1_A := (&models.CellString{}).Stage(stage)
+	__CellString__000003_T2_R1_B := (&models.CellString{}).Stage(stage)
+	__CellString__000004_T2_R2_A := (&models.CellString{}).Stage(stage)
+	__CellString__000005_T2_R2_B := (&models.CellString{}).Stage(stage)
+	__CellString__000006_T2_R3_A := (&models.CellString{}).Stage(stage)
+	__CellString__000007_T2_R3_B := (&models.CellString{}).Stage(stage)
 
-	// Declarations of staged instances of CheckBox
+	__DisplayedColumn__000000_A := (&models.DisplayedColumn{}).Stage(stage)
+	__DisplayedColumn__000001_B := (&models.DisplayedColumn{}).Stage(stage)
+	__DisplayedColumn__000002_Column_1_Boolean := (&models.DisplayedColumn{}).Stage(stage)
+	__DisplayedColumn__000003_Column_2_Float64 := (&models.DisplayedColumn{}).Stage(stage)
+	__DisplayedColumn__000004_Column_3_Icon := (&models.DisplayedColumn{}).Stage(stage)
+	__DisplayedColumn__000005_Column_4_Int := (&models.DisplayedColumn{}).Stage(stage)
+	__DisplayedColumn__000006_Column_5_String := (&models.DisplayedColumn{}).Stage(stage)
 
-	// Declarations of staged instances of DisplayedColumn
-	__DisplayedColumn__000000_A := (&models.DisplayedColumn{Name: `A`}).Stage(stage)
-	__DisplayedColumn__000001_B := (&models.DisplayedColumn{Name: `B`}).Stage(stage)
-	__DisplayedColumn__000002_Column_1_Boolean := (&models.DisplayedColumn{Name: `Column 1 - Boolean`}).Stage(stage)
-	__DisplayedColumn__000003_Column_2_Float64 := (&models.DisplayedColumn{Name: `Column 2 - Float64`}).Stage(stage)
-	__DisplayedColumn__000004_Column_3_Icon := (&models.DisplayedColumn{Name: `Column 3 - Icon`}).Stage(stage)
-	__DisplayedColumn__000005_Column_4_Int := (&models.DisplayedColumn{Name: `Column 4 - Int`}).Stage(stage)
-	__DisplayedColumn__000006_Column_5_String := (&models.DisplayedColumn{Name: `Column 5 - String`}).Stage(stage)
+	__Row__000000_Row_1 := (&models.Row{}).Stage(stage)
+	__Row__000001_Row_2 := (&models.Row{}).Stage(stage)
+	__Row__000002_Table_2_Row_1 := (&models.Row{}).Stage(stage)
+	__Row__000003_Table_2_Row_2 := (&models.Row{}).Stage(stage)
+	__Row__000004_Table_2_Row_3 := (&models.Row{}).Stage(stage)
 
-	// Declarations of staged instances of FormDiv
-
-	// Declarations of staged instances of FormEditAssocButton
-
-	// Declarations of staged instances of FormField
-
-	// Declarations of staged instances of FormFieldDate
-
-	// Declarations of staged instances of FormFieldDateTime
-
-	// Declarations of staged instances of FormFieldFloat64
-
-	// Declarations of staged instances of FormFieldInt
-
-	// Declarations of staged instances of FormFieldSelect
-
-	// Declarations of staged instances of FormFieldString
-
-	// Declarations of staged instances of FormFieldTime
-
-	// Declarations of staged instances of FormGroup
-
-	// Declarations of staged instances of FormSortAssocButton
-
-	// Declarations of staged instances of Option
-
-	// Declarations of staged instances of Row
-	__Row__000000_Row_1 := (&models.Row{Name: `Row 1`}).Stage(stage)
-	__Row__000001_Row_2 := (&models.Row{Name: `Row 2`}).Stage(stage)
-	__Row__000002_Table_2_Row_1 := (&models.Row{Name: `Table 2 - Row 1`}).Stage(stage)
-	__Row__000003_Table_2_Row_2 := (&models.Row{Name: `Table 2 - Row 2`}).Stage(stage)
-	__Row__000004_Table_2_Row_3 := (&models.Row{Name: `Table 2 - Row 3`}).Stage(stage)
-
-	// Declarations of staged instances of Table
-	__Table__000000_EmptyTable := (&models.Table{Name: `EmptyTable`}).Stage(stage)
-	__Table__000001_Table_2 := (&models.Table{Name: `Table 2`}).Stage(stage)
-	__Table__000002_Table_with_5_types := (&models.Table{Name: `Table with 5 types`}).Stage(stage)
+	__Table__000000_EmptyTable := (&models.Table{}).Stage(stage)
+	__Table__000001_Table_2 := (&models.Table{}).Stage(stage)
+	__Table__000002_Table_with_5_types := (&models.Table{}).Stage(stage)
 
 	// Setup of values
 
-	// Cell values setup
 	__Cell__000000_Row_1_Cell_1.Name = `Row 1 - Cell 1`
 
-	// Cell values setup
 	__Cell__000001_Row_1_Cell_2.Name = `Row 1 - Cell 2`
 
-	// Cell values setup
 	__Cell__000002_Row_1_Cell_3.Name = `Row 1 - Cell 3`
 
-	// Cell values setup
 	__Cell__000003_Row_1_Cell_4.Name = `Row 1 - Cell 4`
 
-	// Cell values setup
 	__Cell__000004_Row_1_Cell_5.Name = `Row 1 - Cell 5`
 
-	// Cell values setup
 	__Cell__000005_Row_2_Cell_1.Name = `Row 2 - Cell 1`
 
-	// Cell values setup
 	__Cell__000006_Row_2_Cell_2.Name = `Row 2 - Cell 2`
 
-	// Cell values setup
 	__Cell__000007_Row_2_Cell_3.Name = `Row 2 - Cell 3`
 
-	// Cell values setup
 	__Cell__000008_Row_2_Cell_4.Name = `Row 2 - Cell 4`
 
-	// Cell values setup
 	__Cell__000009_Row_2_Cell_5.Name = `Row 2 - Cell 5`
 
-	// Cell values setup
 	__Cell__000010_T2_R1_A.Name = `T2 - R1 - A`
 
-	// Cell values setup
 	__Cell__000011_T2_R1_B.Name = `T2 - R1 - B`
 
-	// Cell values setup
 	__Cell__000012_T2_R2_A.Name = `T2 - R2 - A`
 
-	// Cell values setup
 	__Cell__000013_T2_R2_B.Name = `T2 - R2 - B`
 
-	// Cell values setup
 	__Cell__000014_T2_R3_A.Name = `T2 - R3 - A`
 
-	// Cell values setup
 	__Cell__000015_T2_R3_B.Name = `T2 - R3 - B`
 
-	// CellBoolean values setup
 	__CellBoolean__000000_Row_1_Cell_1_Cell_False.Name = `Row 1 -Cell 1 - Cell False`
 	__CellBoolean__000000_Row_1_Cell_1_Cell_False.Value = false
 
-	// CellBoolean values setup
 	__CellBoolean__000001_Row_2_Cell_1_Cell_true.Name = `Row 2 - Cell 1 - Cell true`
 	__CellBoolean__000001_Row_2_Cell_1_Cell_true.Value = true
 
-	// CellFloat64 values setup
 	__CellFloat64__000000_Row_1_Cell2_Float.Name = `Row 1 - Cell2 - Float`
 	__CellFloat64__000000_Row_1_Cell2_Float.Value = 20.433333
 
-	// CellFloat64 values setup
 	__CellFloat64__000001_Row_2_Cell_2_Float.Name = `Row 2 - Cell 2 - Float`
 	__CellFloat64__000001_Row_2_Cell_2_Float.Value = 18.550000
 
-	// CellIcon values setup
 	__CellIcon__000000_Row_1_Cell_3_Delete.Name = `Row 1 - Cell 3 - Delete`
 	__CellIcon__000000_Row_1_Cell_3_Delete.Icon = `delete`
 
-	// CellIcon values setup
 	__CellIcon__000001_Row_2_Cell_3_home.Name = `Row 2 - Cell 3 - home`
 	__CellIcon__000001_Row_2_Cell_3_home.Icon = `edit`
 
-	// CellInt values setup
 	__CellInt__000000_Row_1_Cell_4_Int.Name = `Row 1 - Cell 4 - Int`
 	__CellInt__000000_Row_1_Cell_4_Int.Value = 10
 
-	// CellInt values setup
 	__CellInt__000001_Row_2_Cell_4_Int.Name = `Row 2 - Cell 4 - Int`
 	__CellInt__000001_Row_2_Cell_4_Int.Value = 288
 
-	// CellString values setup
 	__CellString__000000_Row_1_Cell_5.Name = `Row 1 - Cell 5`
 	__CellString__000000_Row_1_Cell_5.Value = `Je ferais le métier`
 
-	// CellString values setup
 	__CellString__000001_Row_2_Cell_5.Name = `Row 2 - Cell 5`
 	__CellString__000001_Row_2_Cell_5.Value = `des idoles antiques`
 
-	// CellString values setup
 	__CellString__000002_T2_R1_A.Name = `T2 - R1 - A`
 	__CellString__000002_T2_R1_A.Value = `T2 - R1 - A`
 
-	// CellString values setup
 	__CellString__000003_T2_R1_B.Name = `T2 - R1 - B`
 	__CellString__000003_T2_R1_B.Value = `T2 - R1 - B`
 
-	// CellString values setup
 	__CellString__000004_T2_R2_A.Name = `T2 - R2 - A`
 	__CellString__000004_T2_R2_A.Value = `T2 - R2 - A`
 
-	// CellString values setup
 	__CellString__000005_T2_R2_B.Name = `T2 - R2 - B`
 	__CellString__000005_T2_R2_B.Value = `T2 - R2 - B`
 
-	// CellString values setup
 	__CellString__000006_T2_R3_A.Name = `T2 - R3 - A`
 	__CellString__000006_T2_R3_A.Value = `T2 - R3 - A`
 
-	// CellString values setup
 	__CellString__000007_T2_R3_B.Name = `T2 - R3 - B`
 	__CellString__000007_T2_R3_B.Value = `T2 - R3 - B`
 
-	// DisplayedColumn values setup
 	__DisplayedColumn__000000_A.Name = `A`
 
-	// DisplayedColumn values setup
 	__DisplayedColumn__000001_B.Name = `B`
 
-	// DisplayedColumn values setup
 	__DisplayedColumn__000002_Column_1_Boolean.Name = `Column 1 - Boolean`
 
-	// DisplayedColumn values setup
 	__DisplayedColumn__000003_Column_2_Float64.Name = `Column 2 - Float64`
 
-	// DisplayedColumn values setup
 	__DisplayedColumn__000004_Column_3_Icon.Name = `Column 3 - Icon`
 
-	// DisplayedColumn values setup
 	__DisplayedColumn__000005_Column_4_Int.Name = `Column 4 - Int`
 
-	// DisplayedColumn values setup
 	__DisplayedColumn__000006_Column_5_String.Name = `Column 5 - String`
 
-	// Row values setup
 	__Row__000000_Row_1.Name = `Row 1`
 	__Row__000000_Row_1.IsChecked = true
 
-	// Row values setup
 	__Row__000001_Row_2.Name = `Row 2`
 	__Row__000001_Row_2.IsChecked = false
 
-	// Row values setup
 	__Row__000002_Table_2_Row_1.Name = `Table 2 - Row 1`
 	__Row__000002_Table_2_Row_1.IsChecked = false
 
-	// Row values setup
 	__Row__000003_Table_2_Row_2.Name = `Table 2 - Row 2`
 	__Row__000003_Table_2_Row_2.IsChecked = false
 
-	// Row values setup
 	__Row__000004_Table_2_Row_3.Name = `Table 2 - Row 3`
 	__Row__000004_Table_2_Row_3.IsChecked = false
 
-	// Table values setup
 	__Table__000000_EmptyTable.Name = `EmptyTable`
 	__Table__000000_EmptyTable.HasFiltering = false
 	__Table__000000_EmptyTable.HasColumnSorting = false
@@ -298,7 +202,6 @@ func _(stage *models.StageStruct) {
 	__Table__000000_EmptyTable.SavingInProgress = false
 	__Table__000000_EmptyTable.NbOfStickyColumns = 0
 
-	// Table values setup
 	__Table__000001_Table_2.Name = `Table 2`
 	__Table__000001_Table_2.HasFiltering = false
 	__Table__000001_Table_2.HasColumnSorting = false
@@ -310,7 +213,6 @@ func _(stage *models.StageStruct) {
 	__Table__000001_Table_2.SavingInProgress = false
 	__Table__000001_Table_2.NbOfStickyColumns = 0
 
-	// Table values setup
 	__Table__000002_Table_with_5_types.Name = `Table with 5 types`
 	__Table__000002_Table_with_5_types.HasFiltering = true
 	__Table__000002_Table_with_5_types.HasColumnSorting = true
@@ -356,8 +258,8 @@ func _(stage *models.StageStruct) {
 	__Row__000004_Table_2_Row_3.Cells = append(__Row__000004_Table_2_Row_3.Cells, __Cell__000014_T2_R3_A)
 	__Row__000004_Table_2_Row_3.Cells = append(__Row__000004_Table_2_Row_3.Cells, __Cell__000015_T2_R3_B)
 	__Table__000001_Table_2.DisplayedColumns = append(__Table__000001_Table_2.DisplayedColumns, __DisplayedColumn__000000_A)
-	__Table__000001_Table_2.Rows = append(__Table__000001_Table_2.Rows, __Row__000002_Table_2_Row_1)
 	__Table__000001_Table_2.Rows = append(__Table__000001_Table_2.Rows, __Row__000003_Table_2_Row_2)
+	__Table__000001_Table_2.Rows = append(__Table__000001_Table_2.Rows, __Row__000002_Table_2_Row_1)
 	__Table__000001_Table_2.Rows = append(__Table__000001_Table_2.Rows, __Row__000004_Table_2_Row_3)
 	__Table__000002_Table_with_5_types.DisplayedColumns = append(__Table__000002_Table_with_5_types.DisplayedColumns, __DisplayedColumn__000002_Column_1_Boolean)
 	__Table__000002_Table_with_5_types.DisplayedColumns = append(__Table__000002_Table_with_5_types.DisplayedColumns, __DisplayedColumn__000003_Column_2_Float64)
@@ -367,5 +269,3 @@ func _(stage *models.StageStruct) {
 	__Table__000002_Table_with_5_types.Rows = append(__Table__000002_Table_with_5_types.Rows, __Row__000000_Row_1)
 	__Table__000002_Table_with_5_types.Rows = append(__Table__000002_Table_with_5_types.Rows, __Row__000001_Row_2)
 }
-
-
